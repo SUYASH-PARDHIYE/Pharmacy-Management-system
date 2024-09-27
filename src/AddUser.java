90691a76d3b242a1d625b94fdefff610b2584305
lenovo <lenovo@ShreeSwamiSmarth>
2024-09-28 00:40:05 +0530

    adding empty commit

--- src/AddUser.java
+++ src/AddUser.java
@@ -196,6 +196,7 @@ public class AddUser extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         String userRole = (String) ComboUserRole.getSelectedItem();
+        String name = txtName.getText();
         SimpleDateFormat dFormat = new SimpleDateFormat("dd-MM-YYYY");
         Date date = dateDOB.getDate();
         String dob = "";
